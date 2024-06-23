df039d0e1a6764153a637123750df5aec7ecf0cf
Alexis <fkahope@Mac.local>
2024-06-23 12:26:38 +0200

    Kakao commit 1

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // les config firebase sont cnesés être dans un fichier .env, toutes les clés de sécu doivent etre dan un fichier .env
 const firebaseConfig = {
-  apiKey: "AIzaSyACZNxZXSVZ8Ne7Ui90dNktVgf7v2mHQTo",
-  authDomain: "pd-kakaotalk-d981a.firebaseapp.com",
-  projectId: "pd-kakaotalk-d981a",
-  storageBucket: "pd-kakaotalk-d981a.appspot.com",
-  messagingSenderId: "1059408195097",
-  appId: "1:1059408195097:web:30618bc08aaea258b88488",
-  measurementId: "G-6RN3JG1XN3",
+ 
 };
 
 const firebase = initializeApp(firebaseConfig);
